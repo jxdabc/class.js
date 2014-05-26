744d42b408b7496b5e18b0521d9e25051631bd85
Leo Trol <milstone.jxd@gmail.com>
2014-05-26 17:54:53 +0800

    basics

---
+++ js/code.js
@@ -0,0 +1,15 @@
+$(function(){
+
+	SyntaxHighlighter.defaults['toolbar'] = false;
+
+	$('.code pre').each(function(i,e){
+		var $e = $(e);
+		var url = $e.data('code');
+		$.get(url, function(code){
+			console.log(url, code);
+			$e.text(code);
+			SyntaxHighlighter.highlight(undefined, e);
+		});
+	});
+
+});
